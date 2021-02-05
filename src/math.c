d0d2e7a9fa000c106b3ad6f2f5c326ff1ff8739a
Neeraj121296 <neeraj121296@gmail.com>
2021-02-05 18:12:27 +0530

    Create math.c

---
+++ src/math.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+Use of math.h library functions
+*******************************************************************************/
+
+
+#include <math.h>
+#include <stdio.h>
+
+//int abs(int x);
+//double cos(double x); 
+//double sin(double x);
+//double pow(double X, double Y); == > pow(2,3) === 2^3 = 8
+//double sqrt(double x);
+
+void main()
+{
+   
+  
+   printf("%f\n",cos(60));
+   printf("%f\n",sin(0.5));
+   printf("%d\n",abs(-15)); 
+   printf("%f\n",pow(3.5,2.7));
+   printf("%f",sqrt(16));
+    
+}
